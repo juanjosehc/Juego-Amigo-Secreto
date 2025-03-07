eef4eb5477f7a089e84664dd25413edfc96480b9
Juan Hernandez <razortxz@gmail.com>
2025-03-06 22:32:42 -0500

    función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -10,4 +10,11 @@ function agregarAmigo() {
         document.getElementById('amigo').value = "";
     }
 }
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
 
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}<li>`;
+    }
+}
